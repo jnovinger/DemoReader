d7086a1fa2df0e7c01437a4cdc4378dddaafe8fd
Jason Novinger <jnovinger@gmail.com>
2016-05-01 04:20:01 -0500

    Introduces a new, nest linear layout
    New layout includes separate text views for headline and byline and an
    image view for a thumbnail. Uses the Glide library to retrieve images by
    URL.
    
    Adds new `LeadArt` data class to aid parsing json for lead art urls.

--- app/src/main/java/org/novinger/jason/demoreader/ArticleAdapter.java
+++ app/src/main/java/org/novinger/jason/demoreader/ArticleAdapter.java
@@ -2,10 +2,14 @@ package org.novinger.jason.demoreader;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import org.novinger.jason.demoreader.datamodels.Article;
 
@@ -20,12 +24,16 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView mHeadline;
         public TextView mByline;
+        public ImageView mLeadArt;
+        public Context mContext;
 
-        public ViewHolder(View articleView) {
+        public ViewHolder(View articleView, Context context) {
             super(articleView);
 
             mHeadline = (TextView) articleView.findViewById(R.id.headline);
             mByline = (TextView) articleView.findViewById(R.id.byline);
+            mLeadArt = (ImageView) articleView.findViewById(R.id.lead_art);
+            mContext = context;
         }
     }
 
@@ -38,7 +46,7 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
         View articleView = inflater.inflate(R.layout.article_list_item_view, parent, false);
-        return new ViewHolder(articleView);
+        return new ViewHolder(articleView, context);
     }
 
     @Override
@@ -46,6 +54,7 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
         Article article = mArticles.get(position);
         holder.mHeadline.setText(article.getHeadline());
         holder.mByline.setText(article.getPrimaryAuthor().toString());
+        Glide.with(holder.mContext).load(article.getLeadArtURL()).into(holder.mLeadArt);
     }
 
     @Override
