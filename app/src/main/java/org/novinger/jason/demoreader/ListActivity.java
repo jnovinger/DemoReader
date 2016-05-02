b6cc39a7338bac1d2bfd21a1f282b3bcc7458edc
Jason Novinger <jnovinger@gmail.com>
2016-05-02 11:47:49 -0500

    Add items from DD API feed (#1)
    - Use okhttp to retrive JSON data
    - Use gson to parse JSON into usable objects
    - Modify ListActivity to use feed data rather than static list of string

--- app/src/main/java/org/novinger/jason/demoreader/ListActivity.java
+++ app/src/main/java/org/novinger/jason/demoreader/ListActivity.java
@@ -1,31 +1,53 @@
 package org.novinger.jason.demoreader;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewPropertyAnimator;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+
+import org.novinger.jason.demoreader.datamodels.Article;
+
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class ListActivity extends AppCompatActivity {
+
+    private final static int qty = 50;
+    private final static String feedUrl = "http://www.dailydot.com/api/v1/content/article/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Article[] articles;
+        try {
+            articles = getFeedItems(getFeedUrl());
+        } catch (IOException ex) {
+            articles = new Article[0];
+            Log.d("Exception", ex.toString());
+        }
+
         final ListView listview = (ListView) findViewById(R.id.listview);
-        String[] values = new String[] {
-                "Jason", "Cindy", "Hannah", "Leah", "Kirsten", "Matt", "Elle", "Trevor", "Alex",
-                "Eli", "Danika", "Lori", "Wyatt", "Chad"
-        };
-        final ArrayList<String> list = new ArrayList<String>();
-        Collections.addAll(list, values);
+        final ArrayList<String> list = new ArrayList<>();
+        for (Article article : articles) {
+            list.add(article.toString());
+        }
 
         final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
         listview.setAdapter(adapter);
@@ -33,15 +55,16 @@ public class ListActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
                 final String item = (String) parent.getItemAtPosition(position);
-                view.animate().setDuration(500).alpha(0)
-                    .withEndAction(new Runnable() {
-                        @Override
-                        public void run() {
-                            list.remove(item);
-                            adapter.notifyDataSetChanged();
-                            view.setAlpha(1);
-                        }
-                    });
+                final ViewPropertyAnimator viewPropertyAnimator = view.animate().setDuration(100).alpha(0);
+                viewPropertyAnimator.withEndAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        list.remove(item);
+                        list.trimToSize();
+                        adapter.notifyDataSetChanged();
+                        view.setAlpha(1);
+                    }
+                });
             }
         });
     }
@@ -66,5 +89,23 @@ public class ListActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private Article[] getFeedItems(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder().url(url).build();
+        Response response = client.newCall(request).execute();
+        String json = response.body().string();
+        Gson gson = new Gson();
+        return gson.fromJson(json, Article[].class);
+    }
+
+    private String getFeedUrl() {
+        if(qty > 0) {
+           return feedUrl + "?quantity=" + Integer.toString(qty);
+        }
+
+        return feedUrl;
     }
 }
